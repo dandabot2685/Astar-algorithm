2447732e4f17ec942201955da248c427873c9be9
d@ncodez <danielramparsad1@gmail.com>
2026-09-06 12:19:42 -0400

    Implementation using a numpy array system

--- Astar.py
+++ Astar.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame import Vector2 as vec
 from math import sqrt
 from pygame.locals import*
 from time import perf_counter
 import heapq
 from itertools import count
+import numpy as np
 pygame.init()
 
 HEIGHT = 800
@@ -13,8 +15,6 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 run = True
 font = pygame.font.Font(None,15)
 clock = pygame.time.Clock()
-
-
 
 class move_rect:
     def __init__(self,rect):
@@ -45,149 +45,177 @@ class move_rect:
         self.start_vec = pygame.math.Vector2(self.start)
         self.pos = self.start_vec.copy()
         self.move = True
+        
 class Astar():
-    class options():
-        def __init__(self,x,y,direction,target,curr):
-            self.rect = pygame.Rect(x,y,50,50)
-            self.direction = direction
-            self.parent = direction * -1
-            self.gcost = curr + 30
-            self.hcost = round(sqrt((target.x-x)**2 + (target.y-y)**2)) 
-            self.fcost = self.gcost + self.hcost
-            
-        def Render(self,color):
+    class path_node():
+        def __init__(self,pos,direction,target,curr_g,rect):
+            self.rect = rect
+            self.pos = pos # location on the tilemap
+            self.direction = vec(direction) 
+            self.parent = self.neg_tuple(direction)
+            self.gcost = curr_g + 5 # ground cost from moving to each node
+            self.hcost = (pos[0] - target[0])**2 + (pos[1] - target[1])**2 # calculates the heuristic (distance between two points)
+            self.fcost = self.gcost + self.hcost # sums them together
+
+        def neg_tuple(self,tpl):
+            return tuple(-x for x in tpl)
+        
+        def Render(self,color): #this method is used for debugging
             self.text = font.render(str(self.parent),True,color)
             screen.blit(self.text,(self.rect.x,self.rect.y))
         def __repr__(self):
-            return f'{self.direction} '
-    def __init__(self,rect):
+            return str(self.parent)
+    
+    def __init__(self,tilemap,pos:tuple,rect):
         self.rect = rect
+        self.tilemap = tilemap
+        self.pos = pos #stores (row,col) of the chaser
         self.loop = False       
-        self.directions = {(0,-60):pygame.Vector2(0,-1), #up
-                       (0, 60):pygame.Vector2(0,1),  #down
-                       (60,0):pygame.Vector2(1,0),  #right
-                       (-60,0):pygame.Vector2(-1,0)} #left
-    def setgrid(self):
-        self.grid = []
-        
-        for i in range(-1,2): #generates a 3x3 grid around the chaser and saves it in self.grid
-            for row,rects in tilemap.items():
-                if row == (self.row + i): #as i changes to -1 to 1 it scans the rows above, the same and below of the chaser
-                    for rect in rects:
-                        if (self.rect.x - 60) <= rect.x <= (self.rect.x + 60): #checks for rects between a certain range of the chaser
-                            self.grid.append(rect)
        
+        self.directions = {(0,-1), #UP
+                          (0,1), #DOWN
+                          (1,0), #RIGHT
+                          (-1,0)} #LEFT
         
-        return self.grid
-    def recontruct_path(self,sol):
-        self.sol = [sol[-1].parent * -1]
-        self.pos = pygame.Vector2(self.rect.x,self.rect.y) 
-        self.pos += sol[-1].parent * 60
-        while self.rect != self.start:
-            self.rect.x,self.rect.y = self.pos.x,self.pos.y
-            self.index = sol[sol.index(self.rect)] #basically gives object in sol where it intersects with self.rect
-            self.sol.insert(0,self.index.parent * -1)
-            self.pos += self.index.parent * 60
-        self.sol.pop(0)
-       
+        # keys represent vector directions 
+        # I store them as tuples because vectors are unhashable 
         
-        return self.sol
-        
-        
-                
-            
-        
-        
-    def pathfind(self,target): #recursion boolean allows the method to repeat itself within itself
+   
+    def add_tuples(self,a, b):
+        return tuple(int(x + y) for x, y in zip(a, b))
+    
+   
+    def pathfind(self,target:tuple): #recursion boolean allows the method to repeat itself within itself
         if self.loop == False:
             
-            self.start = self.rect.copy()
-            self.curr = 0
-            self.open = []
-            self.closed = []
+            self.curr_g = 0
+            self.open = [] #these will store tuples (row,col) where the algorithm considers the next path to take
+            self.closed = set() 
             self.sol = []
             self.count = count() # this acts as a tie break for the heapq so that if two nodes have the same fcost then it will choose the one that was added first
-            self.last_pos = self.rect.copy()
+            self.start = self.pos
  
-            while self.rect != target:
+            while not self.pos == target: 
                 #clock.tick(15)
-               
+                
                 self.found = False
                 self.neighbors = []
-                self.row = 1
-                self.y = 50
-
-                #calculates the row where ever it is
-                while self.y != self.rect.y:
-                    self.y += 60
-                    self.row += 1
-                self.grid_3x3 = self.setgrid()
-
-                #calculates each path around the chaser 
-                for (dx,dy),direction in self.directions.items():
-                    self.path = self.options(self.rect.x + dx,self.rect.y + dy,direction,target,self.curr) 
-                    self.collision = 0
-                    for rect in self.grid_3x3: #uses the 3x3 grid too determine which rects do not collide 
-                        if self.path.rect.colliderect(rect): 
-                            self.collision += 1 #there is an obstruction
-                    
-                    if self.collision == 0 and all(self.path.rect.topleft != rect.topleft for rect in self.closed) : #doesnt collide with walls or node is not in the closed list
-                        
-                        self.neighbors.append(self.path)
-                        #pygame.draw.rect(screen,((255,255,255)),self.path.rect)
                 
-                for neighbor in self.neighbors:
+                
+                #calculates each path around the chaser 
+                for direction in self.directions: 
+                    self.is_barrier = self.add_tuples(self.pos,direction[::-1]) 
+                    # since direction represent vector directions which will be stored in self.sol
+                    # it must be reversed so that when i add it to self.pos it works according to the tilemap
+                    
+                    if self.tilemap[self.is_barrier[0],self.is_barrier[1]] !=  1 and not self.is_barrier in self.closed:
+                        offset = (self.rect.x + direction[0]*60,self.rect.y + direction[1]*60)
+                        self.neighbors.append(self.path_node(self.is_barrier,direction,target,self.curr_g,pygame.Rect(offset[0],offset[1],50,50)))
+
+                        #pygame.draw.rect(screen,(255,255,255),pygame.Rect(offset[0],offset[1],50,50))
+
+                                
+                for neighbor in self.neighbors:                    
                     self.found = False
                     for element in self.open:
-                        if neighbor.rect.topleft == element[-1].rect.topleft: #there is a neighbor in the open list
+                        if neighbor.pos == element[-1].pos: #there is a neighbor in the open list
                             self.found = True
                             if element[-1].gcost > neighbor.gcost:  #compares gcost and if there is a better path then updates it           
                                 element[-1].gcost = neighbor.gcost
                                 element[-1].fcost = neighbor.gcost + element[-1].hcost
                                 element[-1].parent = neighbor.direction    
-                    if self.found == False:  #neighbor was not in the open list         
-                        heapq.heappush(self.open, (neighbor.fcost,next(self.count), neighbor))
+                    if self.found == False:  #neighbor was not in the open list   
                         
-                
+                        heapq.heappush(self.open, (neighbor.fcost,next(self.count), neighbor))
+
+
+                             
                 self.lowest = heapq.heappop(self.open) #returns the tuple with the lowest fcost and removes it from the heapq
-                
+
                 #pygame.draw.rect(screen,(0,255,255),self.lowest[2].rect)
 
-                self.curr = self.lowest[2].gcost #obtains the object gcost from the tuple in the heapq
+                self.curr_g = self.lowest[2].gcost #obtains the object gcost from the tuple in the heapq
                 self.sol.append(self.lowest[2]) #adds the object to the solution list
-                self.closed.append(self.rect.copy())
-                self.rect = self.lowest[2].rect #moves rect to the node with the lowest heuristic
-               
+                self.closed.add(self.pos)
+                self.pos = self.lowest[2].pos # moves pos to the node with the lowest heuristic
+
+                #self.rect = self.lowest[2].rect.copy()
                 #pygame.display.flip()
+                               
                 
-            self.loop = True 
-            
+            self.loop = True
             
             return self.recontruct_path(self.sol[:]) #passes in a shallow copy of self.sol as an argument
         else:
             return self.sol
             
-          
+    def recontruct_path(self,sol):
+        self.sol = [sol[-1].direction] #adds the last object.direction
+        self.pos = self.add_tuples(self.pos,sol[-1].parent[::-1]) 
+
+        while not self.pos == self.start:
+            self.index = next((obj for obj in sol if obj.pos == self.pos), None)# gives object in sol where it intersects with self.pos
+            if self.index == None:
+                break
+            self.sol.insert(0,self.index.direction)
+            self.pos = self.add_tuples(self.pos,self.index.parent[::-1])
+            
+        return self.sol
+
+
+def convert_array(map_design):
+    global target
+    arr = []
+    rects = []
+    x,y = 50,50
+    for row in map_design:
+        Row = []
+        for col in row:
+            if col == '#':
+                Row.append(1)
+                rects.append(pygame.Rect(x,y,50,50))
+            elif col == 'T':
+                target = pygame.Rect(x,y,50,50)
+                Row.append(3)
+            elif col == 'x':
+                bot_x,bot_y = x,y
+                Row.append(2)           
+            
+            else:
+                Row.append(0)
+                
+            x += 60
+        arr.append(Row)
+        
+        x = 50
+        y += 60
+        
+    return np.array(arr),rects,bot_x,bot_y
+
+
+       
                 
 
     
 
 class chaser():
-    def __init__(self,x,y,h,w):
+    def __init__(self,x,y,h,w,tilemap,pos):
         self.rect = pygame.Rect(x,y,h,w)
-        self.pos = pygame.Vector2(x,y)
+        self.pos = vec(x,y)
         self.Move = move_rect(self.rect)
         self.moving = False
-        self.shadow = Astar(self.rect) #in order to pathfind again i must redefine this
+        self.shadow = Astar(tilemap,pos,self.rect.copy()) #in order to pathfind again i must redefine this
         self.count = 0
        
         self.speed = 1
         self.skip = False
     def move(self,target):
         self.sol = self.shadow.pathfind(target) #gives a list containing vectors that pathfind to the target
+        
         if self.skip == False:
-            print(str((perf_counter() - start_time)*1000) + 'ms') #shows how fast the algorithm performs
+            print(str((perf_counter() - start_time)*1000) + 'ms')
             self.skip = True
+        
         #clock.tick(144)
         if self.count >= len(self.sol):
             print('finish!')
@@ -314,14 +342,14 @@ model9 = ['##########',
           '##########']
 model10 =    ['###################',
               '#x-------#--------#',
-              '#-##-###-#-###-##-#'
+              '#-##-###-#-###-##-#',
               '#-##-###-#-###-##-#',
               '#-----------------#',
               '#-##-#-#####-#-##-#',
               '#----#---#---#----#',
               '####-###-#-###-####',
               '####-#-------#-####',
-              '####-#-#####-#-####'
+              '####-#-#####-#-####',
               '-------#####-------',
               '####-#---T---#-####',
               '####-#-#####-#-####',
@@ -335,31 +363,18 @@ model10 =    ['###################',
               '###################'
               ]
 
-design = model8
+design = model6
 
-# generates a map of pygame.Rects from the design
-tilemap = {}
-x,y = 50,50
-row = 1
-for line in design:
-    tilemap[row] = []
-    for char in line:
-        if char == '#':
-            tilemap[row].append(pygame.Rect(x,y,50,50)) 
-            # tilemap is stored with keys representing rows and values a list of pygame.Rects (representing walls)
-        elif char == 'x':
-            bot1 = chaser(x, y,50,50) 
-        
-        elif char == 'T':
-            target = pygame.Rect(x,y,50,50)
-        
-        x += 60  
-    row += 1                                  
-    y += 60
-    x = 50
+tilemap_array,tilemap_rects,bot_x,bot_y = convert_array(design)
+# gets coords on tilemap for bot
+x,y = np.where(tilemap_array == 2)
+pos = (x[0],y[0])
 
+bot = chaser(bot_x,bot_y,50,50,tilemap_array,pos)
 
-
+# gets coords on tilemap for target
+x,y = np.where(tilemap_array == 3)
+target_pos = (x[0],y[0])
 
 cooldown = False
 
@@ -371,19 +386,17 @@ while run == True:
     key = pygame.key.get_pressed()
     pygame.draw.rect(screen,((0,255,0)),target)
 
-    for rects in tilemap.values(): #draws all the walls on the screen
-        for rect in rects:
-            pygame.draw.rect(screen,((255,0,0)),rect)
-    
+    for rect in tilemap_rects:
+        pygame.draw.rect(screen,(255,0,0),rect)
 
-    bot1.draw(screen)
+    bot.draw(screen)
     if cooldown == False:
         if key[K_SPACE] == True:
             start_time = perf_counter()
             start = True
 
     if start == True:
-        if bot1.move(target) == True:
+        if bot.move(target_pos) == True:
          
             start = False
             cooldown = True
